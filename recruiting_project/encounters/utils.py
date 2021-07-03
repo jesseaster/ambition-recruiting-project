3e2e1c49bc1c8ca9420379796896ad5d10304035
Jess Easter <jeaster2@my.waketech.edu>
2021-07-03 10:46:14 -0400

    Fixed default parameter value
    Cannot use a list as the default parameter value.

--- recruiting_project/encounters/utils.py
+++ recruiting_project/encounters/utils.py
@@ -3,16 +3,18 @@ from collections import defaultdict
 from recruiting_project.encounters.models import Encounter, Mob
 
 
-def get_encounter_starship_classes(encounter_ids=[]):
+def get_encounter_starship_classes(encounter_ids=None):
     """
     Get a set of starship class names for all encounters
 
     :param encounter_ids: A specific list of encounters to scope this operation, else grab all encounters
     """
     # if not encounter_ids, just get all encounters
-    if not encounter_ids:
-        for encounter in Encounter.objects.all():
-            encounter_ids.append(encounter.id)
+    if encounter_ids is None:
+        encounter_ids = []
+        for e in Encounter.objects.all():
+            encounter_ids.append(e.id)
+
 
     # get unique starship classes for each encounters
     mobs = Mob.objects.filter(
