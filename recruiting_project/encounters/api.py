223b2b7dc3ac84c5018ce711bc3788793674a47c
Jess Easter <jeaster2@my.waketech.edu>
2021-07-03 13:19:05 -0400

    Add filter fields to Starship api`

--- recruiting_project/encounters/api.py
+++ recruiting_project/encounters/api.py
@@ -2,6 +2,13 @@ from rest_framework.viewsets import ModelViewSet
 
 from recruiting_project.encounters.models import Starship, Encounter
 from recruiting_project.encounters.serializers import StarshipSerializer, EncounterSerializer
+from django_filters import rest_framework as filters
+
+
+class StarshipFilter(filters.FilterSet):
+    class Meta:
+        model = Starship
+        fields = ('name', 'starship_class__name')
 
 
 class StarshipsModelViewSet(ModelViewSet):
@@ -10,6 +17,7 @@ class StarshipsModelViewSet(ModelViewSet):
     """
     queryset = Starship.objects.all()
     serializer_class = StarshipSerializer
+    filterset_class = StarshipFilter
 
 
 class EncountersModelViewSet(ModelViewSet):
